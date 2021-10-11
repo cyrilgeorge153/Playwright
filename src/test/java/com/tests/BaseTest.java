4756799eab3e5a58f03bb247f1f2308dc78b7310
Cyril <cyril.george@katzion.com>
2021-10-11 21:30:38 +0530

    cross browser added

--- src/test/java/com/tests/BaseTest.java
+++ src/test/java/com/tests/BaseTest.java
@@ -20,14 +20,33 @@ public class BaseTest {
 	
 	@BeforeMethod
 	public void setUp() {
-		playwright = Playwright.create();
-		chrome = playwright.chromium();
-		browser = chrome
-				.launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(true).setSlowMo(1000));
-		page = browser.newPage();
-		page.navigate("https://www.saucedemo.com/");
-		loginPage=new LoginPage(page);
-		homePage=new HomePage(page);
+		String browserName = System.getProperty("browsername"); 
+		if(browserName.equalsIgnoreCase("chrome"))
+		{
+			playwright = Playwright.create();
+			chrome = playwright.chromium();
+			browser = chrome
+					.launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false).setSlowMo(1000));
+			page = browser.newPage();
+			page.navigate("https://www.saucedemo.com/");
+			loginPage=new LoginPage(page);
+			homePage=new HomePage(page);
+		}
+		else if(browserName.equalsIgnoreCase("headlesschrome"))
+		{
+			playwright = Playwright.create();
+			chrome = playwright.chromium();
+			browser = chrome
+					.launch(new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(true).setSlowMo(1000));
+			page = browser.newPage();
+			page.navigate("https://www.saucedemo.com/");
+			loginPage=new LoginPage(page);
+			homePage=new HomePage(page);
+		}
+		else {
+			System.out.println("browser not found");
+		}
+		
 	}
 	@AfterMethod
 	public void tearDown()
